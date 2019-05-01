d321dec1df2d9ef7915af093c0a790b58e01a166
kumjoo_92 <kumjoo_92@naver.com>
2019-05-01 17:32:13 +0900

    05-01

---
+++ src/main/java/com/osf/web/service/impl/EmpServiceImpl.java
@@ -0,0 +1,35 @@
+package com.osf.web.service.impl;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import com.osf.web.dao.EmpDAO;
+import com.osf.web.service.EmpService;
+
+@Service
+public class EmpServiceImpl implements EmpService {
+
+	@Autowired
+	private EmpDAO edao;
+	
+	@Override
+	public boolean loginEmp(Map<String, String> emp, HttpSession hs) {
+		Map<String,String> dbEmp = edao.selectEmpById(emp);
+		if(dbEmp==null || !dbEmp.get("PWD").equals(emp.get("pwd"))) {
+			return false;
+		}
+		
+		if("2".equals(dbEmp.get("LVL"))){
+			emp.put("lvl", "1");   //2가 최고 관리자고 1이 일반 사용자니까 
+			hs.setAttribute("empList", edao.selectEmpList(emp));
+		}
+		hs.setAttribute("emp", dbEmp);
+		return true;
+	}
+
+}
